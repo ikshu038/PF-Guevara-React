e8f70086c16ff40828efe4d2ed5ab8dbc9da2b76
Santiago Guevara <guevaras090@gmail.com>
2023-07-29 15:38:57 -0300

    error de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
-import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import Cart from "./components/Cart/Cart"
-import NavBar from "./Components/NavBar/NavBar"
+import NavBar from "./components/NavBar/NavBar"
 import { CartProvider } from "./context/CartContext"
 import Checkout from './components/Ckeckout/Checkout'
 
